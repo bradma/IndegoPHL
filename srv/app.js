c1d28fcfd1d0e096b01e431543b82a6ab71aee55
Bradley Martin <twinsen43@B-Rad-2.local>
2016-09-17 16:00:28 -0400

    adjusting server for cors

--- srv/app.js
+++ srv/app.js
@@ -5,10 +5,17 @@ var app = express();
 
 var IndegoSource = require('./logic/data');
 
-app.get('/data/', function(req, res) {
-    IndegoSource.gatherData().then(function(data){
-        res.json(data);
-    });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app
+    .get('/data/', function(req, res) {
+        IndegoSource.gatherData().then(function(data){
+            res.json(data);
+        });
+    });
 
 app.listen(3000, function() { })
